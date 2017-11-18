21cdbfd1a4eb7ee53f01da19daeb9588cc9f6276
Max <mefa2411@gmail.com>
2017-11-18 15:02:37 +0300

    Start

---
+++ Road111/Road111/MainWindow.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Gtk;
+
+public partial class MainWindow : Gtk.Window
+{
+	public static bool progress = false;
+
+	public MainWindow() : base(Gtk.WindowType.Toplevel)
+	{
+		Build();
+	}
+
+	protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+	{
+		Application.Quit();
+		a.RetVal = true;
+	}
+
+	protected void TransportInfo1_clicked(object sender, EventArgs e)
+	{
+		Road111.TransportDialog1 dlg = new Road111.TransportDialog1();
+		dlg.Show();
+	}
+
+	protected void ToggleProgress(object sender, EventArgs e)
+	{
+		progressbar1.Pulse();
+		progressbar2.Pulse();
+		progressbar3.Pulse();
+		progressbar4.Pulse();
+		progressbar5.Pulse();
+	}
+
+}
---
+++ Road111/Road111/TransportDialog1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace Road111
+{
+	public partial class TransportDialog1 : Gtk.Dialog
+	{
+		public TransportDialog1()
+		{
+			this.Build();
+		}
+
+		protected void Ok_Button(object sender, EventArgs e)
+		{
+			this.Destroy();
+		}
+
+		protected void Cancel_Button(object sender, EventArgs e)
+		{
+            this.Destroy();
+		}
+	}
+}
